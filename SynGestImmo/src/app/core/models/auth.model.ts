0ba679e32b4e203f1881e23cebc799d6bb4f6331
markusemile6238 <markusemile6238@gmail.com>
2026-02-03 21:52:32 +0100

    ajout du service auth et token service

--- SynGestImmo/src/app/core/models/auth.model.ts
+++ SynGestImmo/src/app/core/models/auth.model.ts
@@ -1,4 +1,4 @@
-﻿export interface LoginResult{
+﻿export interface LoginRequest{
   email: string;
   password: string;
 }
--- SynGestImmo/src/app/features/auth/login/login.ts
+++ SynGestImmo/src/app/features/auth/login/login.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    MatCardModule
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+
+    this.form = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]]
+    })    
+  }
+
+ submit():void{
+    if(this.form.invalid) return;
+
+    this.auth.login(this.form.value as any).subscribe({
+      next:()=>{
+        this.router.navigateByUrl('/dashboard');
+      },
+      error:()=>{
+        alert('Login incorrect');
+      }
+    })
+  }
+  
+
+
+
 
 }
